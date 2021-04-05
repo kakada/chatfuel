d494838eb66aec3f3adbcdce65ddef2e8db9e701
radin reth <radin.reth@gmail.com>
2021-04-05 17:56:42 +0700

    Import functionalities from static-website

--- app/controllers/districts_controller.rb
+++ app/controllers/districts_controller.rb
@@ -1,22 +1,31 @@
 class DistrictsController < Pumi::DistrictsController
+  include Filterable
+  skip_before_action :check_guisso_cookie
+  before_action :set_query, only: [:index]
+
   def index
-    render(json: Pumi::ResponseSerializer.new(owso_districts))
+    render(json: Pumi::ResponseSerializer.new(pilot_districts))
   end
 
   private
-    def owso_districts
-      district_codes_without_owsu.map do |id|
+    def pilot_districts
+      pilot_district_codes.map do |id|
         Pumi::District.find_by_id(id)
       end
     end
 
     def district_codes
-      Pumi::District.where(province_id: params[:province_id]).map &:id
+      Pumi::District.where(province_id: params[:province_id]).map(&:id)
     end
 
-    def district_codes_without_owsu
-      district_codes.reject do|code| 
+    def pilot_district_codes
+      owso_district_codes = district_codes & @query.district_codes_without_other
+      owso_district_codes.reject do|code| 
         code.match? /#{VariableValue::OWSU_CODE_SUFFIX}$/
       end
+    end
+
+    def set_query
+      @query = DashboardQuery.new(filter_options)
     end
 end
