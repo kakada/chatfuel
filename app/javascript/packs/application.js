53e03a54aee960b2b6414aaa91f8ae9338905773
radin reth <radin.reth@gmail.com>
2021-04-21 15:03:22 +0700

    Embed whole iframe

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,5 @@
 require("@rails/ujs").start();
+global.Rails = Rails;
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
@@ -31,6 +32,8 @@ require("sites/index");
 require("templates/index");
 require("dashboard/show");
 require("home/index");
+require("welcomes/index");
+require("listview/index");
 require("dictionaries/edit");
 require("dictionaries/index");
 
