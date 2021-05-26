c909a91693f427f1e6f146320586b60240f3c87d
radin reth <radin.reth@gmail.com>
2021-05-26 18:11:07 +0700

    UPdate x-frame-options header to allow embed

--- config/application.rb
+++ config/application.rb
@@ -60,6 +60,9 @@ module Chatfuel
     # Rotate Logstasher's log for 30 files with 1Mb each
     logstasher_file = Rails.root.join('log', "logstash_#{Rails.env}.log")
     config.logstasher.logger = Logger.new(logstasher_file, 30, 1.megabyte) if File.exists?(logstasher_file)
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
 
