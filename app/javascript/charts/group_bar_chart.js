bf3119636d9338e84d80abebe6bf757e609309ad
radin reth <radin.reth@gmail.com>
2021-06-15 18:42:55 +0700

    Increase most request chart height

--- app/javascript/charts/group_bar_chart.js
+++ app/javascript/charts/group_bar_chart.js
@@ -2,9 +2,12 @@ import BarChart from "./bar_chart";
 import { suggestedMax } from "../utils/bar_chart";
 
 class GroupBarChart extends BarChart {
-  childOptions =  { legend: {
-                    display: true,
-                    labels: { boxWidth: 12 }}}
+  childOptions = {
+    legend: {
+      display: true,
+      labels: { boxWidth: 12 },
+    },
+  };
   suggestedMax = (data) => suggestedMax(data, 1.5);
   dataset = () => this.format();
 }
