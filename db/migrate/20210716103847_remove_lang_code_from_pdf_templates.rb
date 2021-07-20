b22db80094cae8b7eef908904d3d5577897b5c38
radin reth <radin.reth@gmail.com>
2021-07-20 11:02:44 +0700

    Remove lang_code from pdf template

--- db/migrate/20210716103847_remove_lang_code_from_pdf_templates.rb
+++ db/migrate/20210716103847_remove_lang_code_from_pdf_templates.rb
@@ -1,6 +1,11 @@
 class RemoveLangCodeFromPdfTemplates < ActiveRecord::Migration[6.0]
-  def change
+  def up
     change_column :pdf_templates, :name, :string, default: "", null: true
     remove_column :pdf_templates, :lang_code, :string
+  end
+
+  def down
+    add_column :pdf_templates, :lang_code, :string
+    change_column :pdf_templates, :name, :string, null: false
   end
 end
