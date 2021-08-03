993d0fe24454230d7801b8970a8b3b8389a064f9
radin reth <radin.reth@gmail.com>
2021-08-03 16:17:44 +0700

    load feedbacks

---
+++ app/javascript/charts/citizen-feedback/feedbacks.js
@@ -0,0 +1,11 @@
+import { totalFeedbackByGender } from "./total_feedback_by_gender";
+import { owsoFeedbackTrendByPeriod } from "./owso_feedback_trend_by_period";
+import { overallRatingByOwso } from "./overall_rating_by_owso";
+import { feedbackBySubCategories } from "./feedback_by_sub_categories";
+
+export const feedbacks = [
+  totalFeedbackByGender,
+  owsoFeedbackTrendByPeriod,
+  overallRatingByOwso,
+  feedbackBySubCategories,
+];
--- app/javascript/charts/citizen-feedback/index.js
+++ app/javascript/charts/citizen-feedback/index.js
@@ -1,13 +1,7 @@
-import { totalFeedbackByGender } from "./total_feedback_by_gender";
-import { owsoFeedbackTrendByPeriod } from "./owso_feedback_trend_by_period";
-import { overallRatingByOwso } from "./overall_rating_by_owso";
-import { feedbackBySubCategories } from "./feedback_by_sub_categories";
+import { feedbacks } from "./feedbacks";
 
 export const citizenFeedback = {
   load: function () {
-    totalFeedbackByGender.load();
-    owsoFeedbackTrendByPeriod.load();
-    overallRatingByOwso.load();
-    feedbackBySubCategories.load();
+    feedbacks.forEach((feedback) => feedback.load());
   },
 };
