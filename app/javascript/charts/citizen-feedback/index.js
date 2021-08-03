6b57e6cef188c621c37d033ba1751c942bf9632b
radin reth <radin.reth@gmail.com>
2021-08-03 14:46:45 +0700

    Clean up citizen-feedback/index.js

--- app/javascript/charts/citizen-feedback/index.js
+++ app/javascript/charts/citizen-feedback/index.js
@@ -1,16 +1,9 @@
-import { genderFeedback } from "./gender_feedback_chart";
-import { trendingFeedback } from "./feedback_trend_chart";
-
 import { totalFeedbackByGender } from "./total_feedback_by_gender";
 import { owsoFeedbackTrendByPeriod } from "./owso_feedback_trend_by_period";
 import { overallRatingByOwso } from "./overall_rating_by_owso";
 import { feedbackBySubCategories } from "./feedback_by_sub_categories";
 
 export const citizenFeedback = {
-  // render: function () {
-  //   genderFeedback.render();
-  //   trendingFeedback.render();
-  // },
   load: function () {
     totalFeedbackByGender.load();
     owsoFeedbackTrendByPeriod.load();
