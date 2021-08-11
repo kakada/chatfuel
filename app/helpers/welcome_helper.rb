9bf0d179468e670e35b52f2735cfa1987d700185
radin reth <radin.reth@gmail.com>
2021-08-11 13:42:07 +0700

    Dashboard visitor count

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -18,4 +18,8 @@ module WelcomeHelper
   def described_name
     @location_filter.described_name.presence
   end
+
+  def human_size(num, units={ thousand: 'K', million: 'M' }, format: '%n%u')
+    number_to_human num, units: units, format: format
+  end
 end
