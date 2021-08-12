8e00968c991dda397a65ba5283b64c211afdfc90
radin reth <radin.reth@gmail.com>
2021-08-12 11:48:50 +0700

    Reorder by raw_value

--- app/queries/dashboard_query.rb
+++ app/queries/dashboard_query.rb
@@ -53,8 +53,8 @@ class DashboardQuery
     Session.filter(@options)\
             .joins(step_values: [:variable, :variable_value])\
             .where(step_values: { variable: Variable.user_visit })\
-            .group("mapping_value_#{I18n.locale}", :variable_value_id)\
-            .order(:variable_value_id)\
+            .group("mapping_value_#{I18n.locale}", :raw_value)\
+            .order(:raw_value)\
             .count
   end
 
