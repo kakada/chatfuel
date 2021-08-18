e72307fc4038133d56cb9ceec01699256b3e947f
radin reth <radin.reth@gmail.com>
2021-08-18 14:02:45 +0700

    Keeps state when switch language

--- app/controllers/concerns/query_filter.rb
+++ app/controllers/concerns/query_filter.rb
@@ -2,8 +2,8 @@ class QueryFilter
   def self.before(controller)
     controller.send(:set_daterange)
     controller.send(:t_gon)
+    controller.send(:set_query)
     if controller.request.xhr?
-      controller.send(:set_query)
       controller.send(:set_gon)
       controller.send(:set_active_tab_nav)
     end
