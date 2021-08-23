12382700b5cfacfd9baabca556783b83f0ccd371
radin reth <radin.reth@gmail.com>
2021-08-23 13:54:25 +0700

    Revert migrate_feedback_location

--- db/migrate/20210819102745_migrate_feedback_location_to_sessions.rb
+++
@@ -1,9 +0,0 @@
-class MigrateFeedbackLocationToSessions < ActiveRecord::Migration[6.0]
-  def up
-    Rake::Task['session:migrate_feedback_location:up'].invoke
-  end
-
-  def down
-    Rake::Task['session:migrate_feedback_location:down'].invoke
-  end
-end
