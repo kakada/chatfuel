b94390aecd9a19174143c805731cd41fcae3dc8b
radin reth <radin.reth@gmail.com>
2021-08-24 13:48:16 +0700

    Fixed owsu filter

--- app/models/concerns/session/filterable_concern.rb
+++ app/models/concerns/session/filterable_concern.rb
@@ -5,11 +5,25 @@ module Session::FilterableConcern
 
   module ClassMethods
     def filter(params = {})
+      scope = base_filter(params)
+      scope = scope.where("province_id IN (?) ", ProvinceFilter.fetch(params[:province_id])) if params[:province_id].present?
+      scope = scope.where("district_id IN (?) ", DistrictFilter.fetch(params[:district_id])) if params[:district_id].present?
+      scope
+    end
+
+    def feedback_filter(params = {})
+      scope = base_filter(params)
+      scope = scope.where("feedback_province_id IN (?) ", ProvinceFilter.fetch(params[:province_id])) if params[:province_id].present?
+      scope = scope.where("feedback_district_id IN (?) ", DistrictFilter.fetch(params[:district_id])) if params[:district_id].present?
+      scope
+    end
+
+    private
+
+    def base_filter(params = {})
       scope = all
       scope = scope.where(gender: params[:gender]) if params[:gender].present?
       scope = scope.where(session_type: params[:content_type]) if params[:content_type].present?
-      scope = scope.where("province_id IN (?) ", ProvinceFilter.fetch(params[:province_id])) if params[:province_id].present?
-      scope = scope.where("district_id IN (?) ", DistrictFilter.fetch(params[:district_id])) if params[:district_id].present?
       scope = scope.where(platform_name: params[:platform]) if params[:platform].present?
       scope = scope.where("DATE(sessions.engaged_at) BETWEEN ? AND ?", params[:start_date], params[:end_date]) if params[:start_date].present? && params[:end_date].present?
       scope
