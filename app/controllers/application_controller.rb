cff70ad1a335a5522123e32ae2548324f3fc2330
radin reth <radin.reth@gmail.com>
2021-08-25 16:14:45 +0700

    Reset default url options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,8 +36,4 @@ class ApplicationController < ActionController::Base
     def default_date_format
       Setting.default_date_format
     end
-
-    def default_url_options
-      { locale: I18n.locale }
-    end
 end
