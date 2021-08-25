a635bac54aa4a128aa40fee86cccbcb0636629a0
radin reth <radin.reth@gmail.com>
2021-08-25 17:06:07 +0700

    Fixed embed iframe

--- app/javascript/reports/index.js
+++ app/javascript/reports/index.js
@@ -7,14 +7,6 @@ OWSO.ReportsIndex = (() => {
   function autoSubmit() {
     let form = document.getElementById("q");
 
-    $(form)
-      .on("ajax:before", function () {
-        $(".loading").show();
-      })
-      .on("ajax:complete", function () {
-        $(".loading").hide();
-      });
-
     $.rails.fire(form, "submit");
   }
 
