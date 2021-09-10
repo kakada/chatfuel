3fa0742c030ec9f26644ed5e55c6b00a4466ff0d
radin reth <radin.reth@gmail.com>
2021-09-10 18:04:02 +0700

    Remove visitor count default scope

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -4,7 +4,6 @@ class Ahoy::Visit < ApplicationRecord
   has_many :events, class_name: "Ahoy::Event"
   belongs_to :user, optional: true
 
-  default_scope -> { where("DATE(ahoy_visits.started_at) >= ?", Setting.public_start_date.strftime('%Y/%m/%d')) }
   scope :duration, -> (start_date, end_date) { where("DATE(started_at) BETWEEN ? AND ?", start_date, end_date) }
 
   def self.count_with_fragment start_date, end_date
