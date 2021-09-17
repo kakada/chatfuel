a6a9c8b5333ab902d2115421c9957d22be991c4c
radin reth <radin.reth@gmail.com>
2021-09-17 12:51:34 +0700

    Basic react date ui

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -5,10 +5,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
 
-const Hello = props => (
+const Hello = () => (
   <div>Hello {props.name}!</div>
 )
+
+class App extends React.Component {
+  state = {
+    startDate: moment(this.props.startDate),
+    endDate: moment(this.props.endDate),
+    focusedInput: null
+  }
+
+  render() {
+    console.log(this.props)
+
+    return (
+    <DateRangePicker 
+      startDate={this.state.startDate} // momentPropTypes.momentObj or null,
+      startDateId="your_unique_start_date_id" // PropTypes.string.isRequired,
+      endDate={this.state.endDate} // momentPropTypes.momentObj or null,
+      endDateId="your_unique_end_date_id" // PropTypes.string.isRequired,
+      onDatesChange={({ startDate, endDate }) => this.setState({ startDate, endDate })} // PropTypes.func.isRequired,
+      focusedInput={this.state.focusedInput} // PropTypes.oneOf([START_DATE, END_DATE]) or null,
+      noBorder={true}
+      block={true}
+      small={true}
+      onFocusChange={focusedInput => this.setState({ focusedInput })}/>
+    )
+  }
+}
 
 Hello.defaultProps = {
   name: 'David'
@@ -18,9 +46,13 @@ Hello.propTypes = {
   name: PropTypes.string
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
+  // console.log ( $(".start_date").val() )
   ReactDOM.render(
-    <Hello name="React" />,
-    document.body.appendChild(document.createElement('div')),
+    <App 
+      startDate={$(".start_date").val()}
+      endDate={$(".end_date").val()} />,
+    document.getElementById("react-dates"),
   )
 })
