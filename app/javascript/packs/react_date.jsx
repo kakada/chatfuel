302a4966aedeaa6d21786731fc1abf9ff3dc2507
radin reth <radin.reth@gmail.com>
2021-09-20 10:25:40 +0700

    React date ui with localization

--- app/javascript/packs/react_date.jsx
+++ app/javascript/packs/react_date.jsx
@@ -1,51 +1,37 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "react-dates/initialize";
-import { DateRangePicker } from "react-dates";
-import moment from "moment";
-
-const DEFAULT_DATE_FORMAT = "DD/MM/YYYY"
-const DATE_DISPLAY_FORMAT = "D MMM YYYY"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'react-dates/initialize';
+import { DateRangePicker } from 'react-dates';
+import moment from 'moment';
 
 let currentLocale = $("#q_locale").val()
 moment.locale(currentLocale)
 
 class ReactDate extends React.Component {
   state = {
-    startDate: moment(this.props.startDate, DEFAULT_DATE_FORMAT),
-    endDate: moment(this.props.endDate, DEFAULT_DATE_FORMAT),
+    startDate: moment(this.props.startDate),
+    endDate: moment(this.props.endDate),
     focusedInput: null
-  }
-
-  handleDateChange = ({ startDate, endDate }) => {
-    if(startDate != undefined) $(".start_date").val(this.plainDateFormat(startDate))
-    if(endDate   != undefined) $(".end_date").val(this.plainDateFormat(endDate))
-    this.setState({ startDate, endDate })
-  }
-
-  plainDateFormat(date) {
-    return date.clone().locale("en").format(DEFAULT_DATE_FORMAT);
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.endDate !== this.state.endDate) $("#q").submit()
   }
 
   render() {
     return (
     <DateRangePicker 
+      startDate={this.state.startDate}
+      startDateId="startDate"
+      endDate={this.state.endDate}
+      endDateId="endDate"
+      isOutsideRange={() => null}
+      displayFormat= "DD/MM/YYYY"
+      onDatesChange={({ startDate, endDate }) => {
+        if(startDate != undefined) $(".start_date").val(startDate.format("Y/MM/D"))
+        if(endDate != undefined) $(".end_date").val(endDate.format("Y/MM/D"))
+        this.setState({ startDate, endDate })
+      }}
+      focusedInput={this.state.focusedInput}
+      noBorder={true}
       block={true}
       small={true}
-      noBorder={true}
-      anchorDirection="right"
-      endDateId="endDate"
-      startDateId="startDate"
-      startDate={this.state.startDate}
-      endDate={this.state.endDate}
-      isOutsideRange={() => null}
-      displayFormat={DATE_DISPLAY_FORMAT}
-      onDatesChange={this.handleDateChange}
-      focusedInput={this.state.focusedInput}
       onFocusChange={focusedInput => this.setState({ focusedInput })}/>
     )
   }
