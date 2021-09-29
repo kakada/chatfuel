0b3932cf4ffa63414d0c3ec593e0bc6fa4b5fc9b
radin reth <radin.reth@gmail.com>
2021-09-29 18:39:36 +0700

    Hot fixed feedback trend in district filter

--- app/queries/feedback_trend.rb
+++ app/queries/feedback_trend.rb
@@ -4,35 +4,48 @@ class FeedbackTrend < Feedback
   end
   
   def dataset
-    display_values.map.with_index do |values|
-      id, raw_value, mapping_value = values
+    satisfied.map do |status|
       {
-        label: mapping_value,
-        backgroundColor: colors_mapping[raw_value],
-        data: result_set_mapping.values.map { |raw| raw[mapping_value] || 0 }
+        label: I18n.t(status),
+        backgroundColor: colors_mapping[status],
+        data: result_set_mapping.values.map { |result| result[status] || 0 }
       }
     end
   end
   
   private
+    def colors_mapping
+      satisfied.zip(COLORS).to_h
+    end
+
+    def satisfied
+      VariableValue.value_statuses.keys.reverse
+    end
+
     def result_set_mapping
       return {} unless result_set
 
       result_set.each_with_object({}).with_index do |((key, count), hash), index|
         date, value_id = key
         variable_value = VariableValue.find(value_id)
-        month = month = format_label(date)
+        month = format_label(date)
 
         hash[month] ||= {}
-        hash[month][variable_value.mapping_value] = count
+        prev_count = hash[month][variable_value.value_status].to_i
+        hash[month][variable_value.value_status] = (prev_count + count)
       end
     end
 
     def result_set
+
+      puts @query.options
+      puts @query.province_codes
+      puts @query.district_codes
       Session.feedback_filter(@query.options)\
               .joins(:step_values)\
               .where(step_values: { variable: @variable })\
               .where(feedback_province_id: @query.province_codes)\
+              .where(feedback_district_id: @query.district_codes)\
               .group_by_period(period, :created_at, format: '%b/%y,%Y')\
               .group(:variable_value_id)\
               .count
