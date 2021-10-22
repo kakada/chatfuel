d488b76310d152453e0dccaa066fa5b6c41bf54c
radin reth <radin.reth@gmail.com>
2021-10-22 13:51:47 +0700

    Enforce secure cookies attributes over HTTPS

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,7 @@
-Rails.application.config.session_store :active_record_store, :key => '_chatfuel_session'
+# https://stackoverflow.com/questions/3773605/how-can-i-make-cookies-secure-https-only-by-default-in-rails
 
+Chatfuel::Application.config.session_store( 
+  :cookie_store, 
+  key: '_chatfuel_session',
+  secure: Rails.env.production?
+)
