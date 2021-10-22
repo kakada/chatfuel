162b9173d17fa158c4b7c0d8df71113fd32e7a37
radin reth <radin.reth@gmail.com>
2021-10-22 14:43:53 +0700

    Set ahoy cookie options

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -4,3 +4,6 @@ end
 Ahoy.visit_duration = Setting.visit_duration
 # set to true for JavaScript tracking
 Ahoy.api = false
+
+# https://github.com/ankane/ahoy/issues/436
+Ahoy.cookie_options = {domain: ENV['BASE_URL'], secure: true, httponly: true }
