6830a2ff46dbf074b8514dfe160ab68083d7b590
radin reth <radin.reth@gmail.com>
2021-10-25 15:21:57 +0700

    Update permissions_policy setting
    Setting through Rails config only for rails 6.1.4+

--- config/initializers/permissions_policy.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.permissions_policy do |f|
-  f.camera      :none
-  f.gyroscope   :none
-  f.microphone  :none
-  f.usb         :none
-  f.fullscreen  :self
-  f.payment     :none
-end
