55644714a3b69485e2596b8d4553ec0a2b653d6e
radin reth <radin.reth@gmail.com>
2021-10-25 15:02:15 +0700

    Add secure headers

---
+++ config/initializers/permissions_policy.rb
@@ -0,0 +1,8 @@
+Rails.application.config.permissions_policy do |f|
+  f.camera      :none
+  f.gyroscope   :none
+  f.microphone  :none
+  f.usb         :none
+  f.fullscreen  :self
+  f.payment     :none
+end
