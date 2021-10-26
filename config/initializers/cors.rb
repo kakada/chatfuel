acc71775fa1d3a6601efdf0e1186cfc518f1931d
radin reth <radin.reth@gmail.com>
2021-10-26 10:23:27 +0700

    Check cors only for admin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,9 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
-    resource '/api/*', headers: :any, methods: [:get, :post, :patch, :put, :head]
+    origins   '*'
+    resource  '/api/*',
+              headers: :any,
+              methods: [:get, :post, :patch, :put, :head],
+              if: proc { |env| env['BASE_URL'].match?(/ow4c\.ilabsea\.orgs/) }
   end
 end
