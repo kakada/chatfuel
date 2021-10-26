454c6b144d8191cfca98f536e86941f474d9eba2
radin reth <radin.reth@gmail.com>
2021-10-26 11:54:44 +0700

    Content security update

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -6,18 +6,16 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  if Rails.env.production?
-    policy.default_src :self, :https
-    policy.font_src    :self, :https, :data
-    policy.img_src     :self, :https, :data
-    policy.object_src  :none
-    policy.script_src  :self, :https
-    policy.style_src   :self, :https
-  end
-#   # If you are using webpack-dev-server then specify webpack-dev-server host
-#   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
+  policy.default_src :self, 'localhost', "ilabsea.org"
+  policy.font_src    :self, 'localhost', "ilabsea.org", "fonts.gstatic.com", :data
+  policy.img_src     :self, 'localhost', "ilabsea.org", :data
+  # policy.object_src  :none
+  policy.script_src  :self, :unsafe_inline, "ilabsea.org", "dashboard.ow4c.info", 'localhost'
+  policy.style_src   :self, :unsafe_inline, "fonts.googleapis.com", "ilabsea.org", "dashboard.ow4c.info", 'localhost'
+  # If you are using webpack-dev-server then specify webpack-dev-server host
+  # policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
-#   # Specify URI for violation reports
+  # Specify URI for violation reports
   # policy.report_uri "/csp-violation-report-endpoint"
 end
 
@@ -30,4 +28,4 @@ end
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy-Report-Only
-Rails.application.config.content_security_policy_report_only = true
+# Rails.application.config.content_security_policy_report_only = true
