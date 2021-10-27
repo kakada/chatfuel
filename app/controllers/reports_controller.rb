f450a277b3f977b45617372cd18a6ea2477bfc4c
radin reth <radin.reth@gmail.com>
2021-10-27 17:30:26 +0700

    Clean up unused comment

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,3 @@
 class ReportsController < WelcomesController
   layout 'welcome'
-  content_security_policy_report_only only: :index
 end
--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,17 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins   '*'
-    resource  '*',
-              headers: :any,
-              methods: [:get, :options],
-              expose: ['Access-Control-Allow-Origin']
-  end
-
-  allow do
-    origins   '*'
     resource  '/api/*',
               headers: :any,
-              methods: [:get, :post, :patch, :put, :head],
-              if: proc { |env| env['BASE_URL'].match?(/ow4c\.ilabsea\.orgs/) }
+              methods: [:get, :post, :patch, :put, :head]
   end
 end
