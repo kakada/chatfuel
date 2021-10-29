2246d8cfa930f1c399b2c4213564dd72b2455dd4
radin reth <radin.reth@gmail.com>
2021-10-29 09:19:17 +0700

    Allow google analytic for image src in CSP

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -8,7 +8,7 @@
 Rails.application.config.content_security_policy do |policy|
   policy.default_src :self, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s
   policy.font_src    :self, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s, "fonts.gstatic.com", :data
-  policy.img_src     :self, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s, "https://www.facebook.com", :data
+  policy.img_src     :self, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s, "https://www.facebook.com", "https://www.google-analytics.com", :data
   policy.script_src  :self, :unsafe_inline, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s, "*.fontawesome.com", "https://www.google-analytics.com", "connect.facebook.net", "https://web.facebook.com", "https://www.facebook.com"
   policy.style_src   :self, :unsafe_inline, ENV["LOCAL_DOMAIN"].to_s, ENV["PRODUCTION_DOMAIN"].to_s, "*.fontawesome.com", "fonts.googleapis.com"
   policy.connect_src :self, :unsafe_inline, ENV["LOCAL_DOMAIN"].to_s, "https://web.facebook.com", "https://www.facebook.com", "https://www.google-analytics.com"
