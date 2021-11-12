c7374ea62aa0067f285a75ff811b4b84490fe1f6
radin reth <radin.reth@gmail.com>
2021-11-12 17:45:06 +0700

    Hide other districts in pilot provinces

--- app/models/variable_value.rb
+++ app/models/variable_value.rb
@@ -27,6 +27,7 @@ class VariableValue < ApplicationRecord
   enum value_status: { like: "0", acceptable: "1", dislike: "2" }
   USER_FEEDBACK = 'user_feedback'
   OWSU_CODE_SUFFIX = '99'
+  OTHER_CODE_SUFFIX = '00'
 
   # associations
   belongs_to :variable
