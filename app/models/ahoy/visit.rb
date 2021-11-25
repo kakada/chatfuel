191ceb3cffcfdad5d0f96466b23d390cea967f17
radin reth <radin.reth@gmail.com>
2021-11-25 14:01:21 +0700

    Migrate inappropriate ahoy visits
    Delete ahoy visits in a specify duration

--- app/models/ahoy/visit.rb
+++ app/models/ahoy/visit.rb
@@ -9,4 +9,39 @@ class Ahoy::Visit < ApplicationRecord
   def self.count_with_fragment start_date, end_date
     [duration(start_date.to_date, end_date.to_date).count, all.count]
   end
+
+  def self.clear(scope_ids)
+    Ahoy::Visit.where(id: scope_ids).delete_all
+    Ahoy::Event.where(visit_id: scope_ids).delete_all
+  end
+
+  def self.from(started_at, ips = [])
+    scope = where("started_at > ?", started_at)
+    scope = where("ip in (?)", ips) if ips.present?
+    scope.order(:ip, :started_at)
+  end
+
+  def self.duplicate_ids_within_period(options = {})
+    deleted_ids = []
+    started_ip = started_at = nil
+    find_each do |visit|
+      if started_ip == visit.ip
+        if visit.less_than? started_at + options[:duration].to_i.minutes
+          deleted_ids << visit.id 
+        else
+          started_at = visit.started_at
+        end
+      else
+        started_ip = visit.ip
+        started_at = visit.started_at
+      end
+    end
+
+    deleted_ids
+  end
+
+  def less_than? datetime
+    started_at < datetime
+  end
+
 end
--- spec/factories/visit.rb
+++ spec/factories/visit.rb
@@ -1,4 +1,9 @@
 FactoryBot.define do
   factory :visit, class: Ahoy::Visit do
+    ip { FFaker::Internet.ip_v4_address }
+
+    trait :random_grace_period do
+      started_at { 1.minute.from_now }
+    end
   end
 end
